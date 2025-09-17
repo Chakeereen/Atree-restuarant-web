5e7b6062e76581c86251e97fa1b1a1dcfe787c01
Chakeereen 65143052 <chakeereen606@gmail.com>
2025-09-17 16:00:31 +0700

    fix staff login 1

--- src/app/api/auth/staff/route.ts
+++ src/app/api/auth/staff/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: NextRequest) {
         },
       });
 
-      return NextResponse.json({ error: "Staff not found" }, { status: 404 });
+      return NextResponse.json({ error: "Staff not found" }, { status: 401 });
     }
 
     // ตรวจสอบ password
