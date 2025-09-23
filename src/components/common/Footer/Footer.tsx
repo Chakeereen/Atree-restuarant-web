e9d535e976047dca25cb5f9862adcdfb8030186e
Chakeereen 65143052 <chakeereen606@gmail.com>
2025-09-23 19:34:44 +0700

    assdasdasd

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full bg-secondary text-secondary-foreground p-4 text-center">
-      &copy; {new Date().getFullYear()} My Company. All rights reserved.
+      &copy; {new Date().getFullYear()} ATREE Halal Food. All rights reserved.
     </footer>
   );
 }
